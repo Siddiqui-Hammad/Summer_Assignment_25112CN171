5a9830a3a7464553a044cecd34fc5b3711ecf9d9
Siddiqui-Hammad <hammad.sidd2906.com>
2026-06-09 15:50:24 +0530

    Day~9

---
+++ Day~9/Q36.c
@@ -0,0 +1,29 @@
+/*Write a program to Print hollow square 
+pattern. 
+*   **** 
+*      * 
+*      * 
+*      * 
+* ****                            */
+
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    printf("Enter size of square: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= n; j++) {
+            // Print * for first row, last row, first column, last column
+            if(i == 1 || i == n || j == 1 || j == n) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
