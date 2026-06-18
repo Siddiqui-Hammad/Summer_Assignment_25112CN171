efdba7bbfd552cd190cfb860e76f6c07e9e55972
Siddiqui-Hammad <hammad.sidd2906.com>
2026-06-18 15:56:18 +0530

    Day-18

---
+++ Day-18/Q69.c
@@ -0,0 +1,34 @@
+//Write a program to Bubble sort. 
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // bubble sort logic
+    for(int i = 0; i < n-1; i++) {
+        for(int j = 0; j < n-1-i; j++) {
+            if(arr[j] > arr[j+1]) {
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array (ascending): ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
